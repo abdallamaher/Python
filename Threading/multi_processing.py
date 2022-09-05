5dbc3efe85493ac1d3de486323553d27392104d0
Abdallah Maher <abdallahmaher@Abdallahs-MacBook-Air.local>
2022-09-05 14:50:08 +0200

    Python Threading

---
+++ Threading/multi_processing.py
@@ -0,0 +1,38 @@
+# *********************************
+# Python multiprocessing
+# *********************************
+# multiprocessing = running tasks in parallel on different cpu cores, bypasses GIL used for threading
+#                   multiprocessing = [ Parallel   ] better for cpu bound tasks (heavy cpu usage)
+#                   multithreading  = [ Take turns ] better for io bound tasks (waiting around)
+
+from multiprocessing import Process, cpu_count
+import time
+
+
+def counter(num):
+    count = 0
+    while count < num:
+        count += 1
+
+
+def main():
+
+    print("cpu count:", cpu_count())
+
+    a = Process(target=counter, args=(500000000,))
+    b = Process(target=counter, args=(500000000,))
+
+    a.start()
+    b.start()
+
+    print("processing...")
+
+    a.join()
+    b.join()
+
+    print("Done!")
+    print("finished in:", time.perf_counter(), "seconds")
+
+
+if __name__ == '__main__':
+    main()
